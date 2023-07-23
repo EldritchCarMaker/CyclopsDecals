c7c962fc858a82d8de8198abe0cc93a20fe73fc0
Nagorrogan <eldritchcarmaker@gmail.com>
2023-07-23 02:58:48 -0500

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CyclopsDecals.Patches;
+using BepInEx;
+using BepInEx.Logging;
+
+namespace CyclopsDecals
+{
+    [BepInPlugin("EldritchCarMaker.CyclopsDecals", "Cyclops decals", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        private static ManualLogSource _laggr;
+        public enum Level//lazy fix
+        {
+            Info,
+            Debug,
+            Warn,
+            Error
+        }
+        public static void Log(Level level, object obj)
+        {
+            _laggr.Log(levelConvert[level], obj);
+        }
+        private static Dictionary<Level, LogLevel> levelConvert = new Dictionary<Level, LogLevel>()
+        {
+            { Level.Info, LogLevel.Info },
+            { Level.Error, LogLevel.Error },
+            { Level.Warn, LogLevel.Warning },
+            { Level.Debug, LogLevel.Debug }
+        };
+
+        public void Awake()
+        {
+            _laggr = Logger;
+            Log(Level.Info, "Loading CyclopsDecals");
+
+            var assetsPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Assets");
+
+            foreach (var file in Directory.GetFiles(assetsPath))
+            {
+                CyclopsPatch.decalFiles.Add(Path.Combine(assetsPath, file));
+            }
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), "CyclopsDecalsHarmonyID");
+
+            Log(Level.Info, "Loaded CyclopsDecals");
+        }
+    }
+}
---
+++ Patches/CyclopsPatch.cs
@@ -0,0 +1,47 @@
+﻿using Sentry;
+using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using Logger = CyclopsDecals.Main;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace CyclopsDecals.Patches
+{
+    [HarmonyPatch(typeof(SubRoot))]
+    internal class CyclopsPatch
+    {
+        public static List<string> decalFiles = new List<string>();
+
+        [HarmonyPatch(nameof(SubRoot.Awake))]
+        public static void Prefix(SubRoot __instance)
+        {
+            if(!__instance.name.Contains("(Clone)"))
+            {
+                Logger.Log(Logger.Level.Info, "Skipping prefab");
+                return;
+            }
+
+            Logger.Log(Logger.Level.Info, $"Adding decals to cyclops. {decalFiles.Count} decals total");
+            var subname = __instance.GetComponentInChildren<SubName>(true);
+            var nameText = subname.hullName;
+
+            foreach(var decalFile in decalFiles)
+            {
+                var decalName = decalFile.Substring((decalFile.Length / 3) * 2);
+                Logger.Log(Logger.Level.Info, $"Adding Decal {decalName}");
+                var decal = Nautilus.Utility.ImageUtils.LoadTextureFromFile(decalFile);//file name includes file extension already
+                var decalObj = GameObject.Instantiate(nameText.gameObject, nameText.transform.position, nameText.transform.rotation, nameText.transform.parent);
+                decalObj.name = $"Decal {decalName}";
+                GameObject.DestroyImmediate(decalObj.GetComponent<TextMeshProUGUI>());
+                decalObj.AddComponent<Image>().sprite = Sprite.Create(decal, new Rect(0, 0, decal.width, decal.height), Vector2.zero);
+                decalObj.transform.position = nameText.transform.position;
+                decalObj.transform.rotation = nameText.transform.rotation;
+                decalObj.transform.localScale = nameText.transform.localScale;
+
+                decalObj.transform.position += 0.15f * decalObj.transform.up;//bottom was clipping with the cyclops
+            }
+        }
+    }
+}
